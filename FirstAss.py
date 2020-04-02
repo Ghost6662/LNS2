c2ee1f68e0eb3bac732a08b8097d7153569243ba
Ghost6662 <emil.eminov.95@gmail.com>
2020-04-02 07:13:28 +0200

    Done with Basic Classes

--- FirstAss.py
+++ FirstAss.py
@@ -57,3 +57,78 @@ First_Racer.crash(Second_Racer)
 
 print(Second_Racer.pace)
 print(Second_Racer.health)
+
+# Additional Self Part
+print("----------Additional Self Part----------")
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.power = 100
+        self.power_reduce = 30
+        self.thirst = 100
+        self.thirst_reduce = 40
+        self.hunger = 0
+        self.hunger_increase = 50
+    def readbook(self,pers):
+        pers.power = pers.power - self.power_reduce
+        pers.thirst = pers.thirst - self.thirst_reduce
+        pers.hunger = pers.hunger + self.hunger_increase
+    def relax (self, pers):
+        pers.power = 90
+        pers.thirst = pers.thirst + 20
+        pers.hunger = pers.hunger - 15
+
+First_Reader = Person("Emil Eminov")
+
+First_Reader.readbook(First_Reader)
+print("----------After Reading----------")
+print(First_Reader.name)
+print(First_Reader.power)
+print(First_Reader.thirst)
+print(First_Reader.hunger)
+
+First_Reader.relax(First_Reader)
+print("----------After Relaxing----------")
+print(First_Reader.name)
+print(First_Reader.power)
+print(First_Reader.thirst)
+print(First_Reader.hunger)
+
+print("----------4th Part----------")
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.power = 100
+        self.power_reduce = 30
+        self.thirst = 100
+        self.thirst_reduce = 40
+        self.hunger = 0
+        self.hunger_increase = 50
+        self.book_number = 200
+    def __str__(self):
+        return "Name:{} ->> Power:{}, Thirst:{}, Hunger:{}, Book Number:{}".format(self.name, self.power, self.thirst, self.hunger, self.book_number)
+    def readbook(self,pers):
+        pers.power = pers.power - self.power_reduce
+        pers.thirst = pers.thirst - self.thirst_reduce
+        pers.hunger = pers.hunger + self.hunger_increase
+        pers.book_number -= 1
+        print("After one reading session {} has {} power left, his thirst Level reduces to {}, Hunger level increases {}, and {} books left").format(self.name, self.power, self.thirst, self.hunger, self.book_number)
+    def relax (self, pers):
+        pers.power = 90
+        pers.thirst = pers.thirst + 20
+        pers.hunger = pers.hunger - 15
+        print("After relaxing {}'s power increases to {} , his thirst Level increases to {}, Hunger level decreases {}").format(self.name, self.power, self.thirst, self.hunger)
+
+
+First_Reader = Person("Emil Eminov")
+
+print(First_Reader)
+print ("\n")
+
+First_Reader.readbook(First_Reader)
+print(First_Reader)
+print ("\n")
+
+First_Reader.relax(First_Reader)
+print(First_Reader)
+
