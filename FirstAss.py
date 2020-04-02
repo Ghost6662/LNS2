45942cde7aa483f91a309c50dd3f24f268980638
Ghost6662 <emil.eminov.95@gmail.com>
2020-04-02 06:17:59 +0200

    Done until 4th Part

--- FirstAss.py
+++ FirstAss.py
@@ -1,3 +1,6 @@
+print("----------------------------------Learn Python OOP with CleverProgrammer----------------------------------")
+print("----------1st Part----------")
+
 class Restaurant:
     def __init__(self, name, location, Price, Distance):
         self.name = name
@@ -21,4 +24,36 @@ print('Price of Second Restaurant is: ' + Second_Restaurant.Price)
 print('Distance to First Restaurant is: ' + str(First_Restaurant.Distance) + ' miles')
 print('Distance to Second Restaurant is: ' + str(Second_Restaurant.Distance) + ' miles')
 
-print('Distance between First and Second Restaurant: ' + str(First_Restaurant.Distance - Second_Restaurant.Distance) + ' miles')
+print('Distance btw 1st and 2nd Restaurant: ' + str(First_Restaurant.Distance - Second_Restaurant.Distance) + ' miles')
+
+# Second Part
+print("----------2nd Part----------")
+
+class Course:
+    website = "http://bestcourse.com"
+    def __init__(self, name):
+        self.name = name
+
+Py_Course = Course("PY")
+print(Py_Course.name)
+print(Course.website)
+
+print("----------3rd Part----------")
+
+class StreetRace:
+    def __init__(self, name):
+        self.name = name
+        self.pace = 100
+        self.health = 100
+        self.hit = 60
+    def crash(self, racer):
+        racer.pace = 0
+        racer.health = racer.health - self.hit
+
+First_Racer = StreetRace("Mike")
+Second_Racer = StreetRace("Dany")
+
+First_Racer.crash(Second_Racer)
+
+print(Second_Racer.pace)
+print(Second_Racer.health)
